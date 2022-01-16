07efe54f596a36eec44285a20f1f4e1577eb7601
SiriusLL <sirius.love.light@gmail.com>
2022-01-16 10:02:34 -0700

    compile contract, write deply script, deploy

---
+++ scripts/deploy.js
@@ -0,0 +1,16 @@
+const { ethers } = require("hardhat");
+
+async function Main() {
+  const HelloWorld = await ethers.getContractFactory("HelloWorld");
+
+  const hello_world = await HelloWorld.deploy("Hello World");
+
+  console.log('Contract deployed to address', hello_world.address);
+}
+
+Main()
+  .then(() => process.exit(0))
+  .catch(error =>{
+    console.log(error);
+    process.exit(1);
+  });
